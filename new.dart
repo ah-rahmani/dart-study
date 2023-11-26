82251938f37959eb2ce6aa98c1909f7291aece8b
rahmani <98764031+ah-rahmani@users.noreply.github.com>
2023-11-26 20:36:04 +0330

    Add files via upload

---
+++ new.dart
@@ -0,0 +1,46 @@
+// void main() {
+//   username(firstname: ' ', lastname: " ");
+// }
+
+// void username({String? firstname, String? lastname}) {
+//   print("$firstname"+"$lastname");
+// }
+// void main() {
+//   var ferari = car();
+//   ferari.color = 'blue';
+//   ferari.speed = 360;
+//   ferari.model = '2023';
+//   ferari.start();
+//   ferari.pelak = "E365F52";
+  
+//   ferari.printpelak();
+
+
+//   var bmw = car();
+//     bmw.pelak = "R452G63";
+  
+  
+//   print(bmw.pelak);
+
+  
+//   bmw.stop();
+// }
+
+// class car {
+//   String? color;
+//   int? speed;
+//   String? model;
+//   String? pelak;
+
+//   void start() {
+//     print('start car');
+//   }
+
+//   void printpelak() {
+//     print(this.pelak);
+//   }
+
+//   void stop() {
+//     print('stop car');
+//   }
+// }
